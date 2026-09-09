d23745c0ee2815846429c215660872aabdfe903c
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-09-09 17:34:21 +0200

    [Add] Movement Recover

---
+++ Assets/Scripts/Runtime/Inputs/PlayerInputCapture.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace InputCapture
+{
+    public class PlayerInputCapture : MonoBehaviour
+    {
+        private static PlayerInputCapture instance;
+        public static PlayerInputCapture Instance
+        {
+            get => instance;
+        }
+
+        private PlayerInput _inputAction;
+
+        private void Awake() {
+            if (Instance != null && Instance != this) {
+                Destroy(this);
+                return;
+            }
+            else instance = this;
+        }
+
+        public event Action OnP1MoveStart;
+        public event Action OnP1MoveEnd;
+
+        public event Action OnP2MoveStart;
+        public event Action OnP2MoveEnd;
+
+        private void OnEnable() {
+            _inputAction = new PlayerInput();
+            _inputAction.Gameplay.Enable();
+            
+            _inputAction.Gameplay.MovementPlayer1.performed += ctx => OnP1MoveStart.Invoke();
+            _inputAction.Gameplay.MovementPlayer1.canceled += ctx => OnP1MoveEnd.Invoke();
+
+            _inputAction.Gameplay.MovementPlayer2.performed += ctx => OnP2MoveStart.Invoke();
+            _inputAction.Gameplay.MovementPlayer2.canceled += ctx => OnP2MoveEnd.Invoke();
+        }
+        
+        
+    }
+}
