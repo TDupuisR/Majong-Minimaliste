695052ba6ab4115e1d727a627be228a32acd562e
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-09-10 10:18:33 +0200

    [Fix] Vector2 on input Actions

--- Assets/Scripts/Runtime/Inputs/PlayerInputCapture.cs
+++ Assets/Scripts/Runtime/Inputs/PlayerInputCapture.cs
@@ -22,21 +22,21 @@ namespace InputCapture
             else instance = this;
         }
 
-        public event Action OnP1MoveStart;
-        public event Action OnP1MoveEnd;
+        public event Action<Vector2> OnP1MoveStart;
+        public event Action<Vector2> OnP1MoveEnd;
 
-        public event Action OnP2MoveStart;
-        public event Action OnP2MoveEnd;
+        public event Action<Vector2> OnP2MoveStart;
+        public event Action<Vector2> OnP2MoveEnd;
 
         private void OnEnable() {
             _inputAction = new PlayerInput();
             _inputAction.Gameplay.Enable();
             
-            _inputAction.Gameplay.MovementPlayer1.performed += ctx => OnP1MoveStart.Invoke();
-            _inputAction.Gameplay.MovementPlayer1.canceled += ctx => OnP1MoveEnd.Invoke();
+            _inputAction.Gameplay.MovementPlayer1.performed += ctx => OnP1MoveStart.Invoke(ctx.ReadValue<Vector2>());
+            _inputAction.Gameplay.MovementPlayer1.canceled += ctx => OnP1MoveEnd.Invoke(Vector2.zero);
 
-            _inputAction.Gameplay.MovementPlayer2.performed += ctx => OnP2MoveStart.Invoke();
-            _inputAction.Gameplay.MovementPlayer2.canceled += ctx => OnP2MoveEnd.Invoke();
+            _inputAction.Gameplay.MovementPlayer2.performed += ctx => OnP2MoveStart.Invoke(ctx.ReadValue<Vector2>());
+            _inputAction.Gameplay.MovementPlayer2.canceled += ctx => OnP2MoveEnd.Invoke(Vector2.zero);
         }
 
         private void OnDisable()
